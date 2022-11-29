65927ae2098c032e709398b271692dd1738a2858
shinderohit127 <shinderohit127@gmail.com>
2022-11-29 21:21:21 +0530

    first iteration landing UI

---
+++ sld_project_app/lib/screens/landing/landing_screen_animation.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:sld_project_app/screens/landing/components/care_view.dart';
+import 'package:sld_project_app/screens/landing/components/center_next_button.dart';
+import 'package:sld_project_app/screens/landing/components/mood_diary_view.dart';
+import 'package:sld_project_app/screens/landing/components/relax_view.dart';
+import 'package:sld_project_app/screens/landing/components/splash_view.dart';
+import 'package:sld_project_app/screens/landing/components/top_back_skip_view.dart';
+import 'package:sld_project_app/screens/landing/components/welcome_view.dart';
+
+class LandingScreenAnimation extends StatefulWidget {
+  const LandingScreenAnimation({super.key});
+
+  @override
+  State<LandingScreenAnimation> createState() => _LandingScreenAnimationState();
+}
+
+class _LandingScreenAnimationState extends State<LandingScreenAnimation>
+    with TickerProviderStateMixin {
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 8));
+    _animationController?.animateTo(0.0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF7EBE1),
+      body: ClipRect(
+        child: Stack(
+          children: [
+            SplashView(
+              animationController: _animationController!,
+            ),
+            RelaxView(
+              animationController: _animationController!,
+            ),
+            CareView(
+              animationController: _animationController!,
+            ),
+            MoodDiaryView(
+              animationController: _animationController!,
+            ),
+            WelcomeView(
+              animationController: _animationController!,
+            ),
+            TopBackSkipView(
+              onBackClick: _onBackClick,
+              onSkipClick: _onSkipClick,
+              animationController: _animationController!,
+            ),
+            CenterNextButton(
+              animationController: _animationController!,
+              onNextClick: _onNextClick,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onSkipClick() {
+    _animationController?.animateTo(0.8,
+        duration: const Duration(milliseconds: 1200));
+  }
+
+  void _onBackClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.0);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.2);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.8 &&
+        _animationController!.value <= 1.0) {
+      _animationController?.animateTo(0.8);
+    }
+  }
+
+  void _onNextClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.8);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _signUpClick();
+    }
+  }
+
+  void _signUpClick() {}
+}
